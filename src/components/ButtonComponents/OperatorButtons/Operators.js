b96eaf0d8c93a33521a03607b3e5b87b9514351d
imxande <alerodriguezlarosa@gmail.com>
2019-11-05 18:59:22 -0500

    displaying a calculator but is not functional yet

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -11,13 +11,13 @@ const Operators = () => {
   // STEP 2 - add the imported data to state
   const [operatorState, setOperatorState] = useState(operators);
   return (
-    <div>
+    <div className="operators">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {operatorState.map((button, index) => {
-         <OperatorButton key = {index} button={button}/>
-       })}
+       {operatorState.map((button, index) => (
+         <OperatorButton key={index} button={button}/>
+  ))}
     </div>
   );
 };
